71c0c679cf282f22c042a46215e00f81e7e84e77
gnd <gnd@itchybit.org>
2019-11-12 00:38:41 +0100

    small fixes

--- find_and_convert.sh
+++ find_and_convert.sh
@@ -25,6 +25,10 @@ do
 	echo $RAWNAME"_720p.mp4" >> $WEB_ROOT/wp-content/uploads/converted
 	echo $RAWNAME".mp4" >> $WEB_ROOT/wp-content/uploads/converted
 	echo $RAWNAME".ogg" >> $WEB_ROOT/wp-content/uploads/converted
+
+	# Announce conversion
+	sendemail -o tls=yes -s $SERVER:$PORT -xu $USER -xp $PASS -f $FROM -t $TO -u "New video to be converted: $RAWNAME" -m "No further text."
+
 	cd $DIR
 	$WEB_ROOT/wp-content/themes/allyoucan/convert.sh $FILE
 
