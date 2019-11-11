c8f8b7f87302d2ecad7635f3d5744f016d185290
gnd <gnd@itchybit.org>
2019-11-12 00:35:43 +0100

    small fixes

--- find_and_convert.sh
+++ find_and_convert.sh
@@ -27,7 +27,7 @@ do
 	echo $RAWNAME".ogg" >> $WEB_ROOT/wp-content/uploads/converted
 	cd $DIR
 	$WEB_ROOT/wp-content/themes/allyoucan/convert.sh $FILE
-done
 
-# Report conversion
-sendemail -o tls=yes -s $SERVER:$PORT -xu $USER -xp $PASS -f $FROM -t $TO -u "New video converted: $RAWNAME" -m "No further text."
+	# Report conversion
+	sendemail -o tls=yes -s $SERVER:$PORT -xu $USER -xp $PASS -f $FROM -t $TO -u "New video converted: $RAWNAME" -m "No further text."
+done
