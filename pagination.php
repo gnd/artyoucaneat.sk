c4cda1e4582955136d54589dd0d7baa38e38000b
gnd <gnd@itchybit.org>
2020-01-25 23:43:43 +0100

    hiding pagination until mobile fixed

--- pagination.php
+++ pagination.php
@@ -15,5 +15,7 @@
 $pagination_string = get_the_posts_pagination(array('screen_reader_text' => ' ','next_text' => '>>','prev_text' => '<<'));
 ?>
 <div id="pagination_container">
+    <!-- HIDDEN FOR NOW - need to fix the mobile version first
     <?php echo $pagination_string; ?>
+    -->
 </div>
