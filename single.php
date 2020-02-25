d2b7c4ccafd92c360ce3f380c581f48cb23c7731
gnd <gnd@itchybit.org>
2020-02-25 10:40:13 +0100

    facebook loader too slow, debugging

--- single.php
+++ single.php
@@ -88,7 +88,7 @@ $translation_links = process_persons($translation, "translation");
      <?php
          $og_desc = get_post_meta($current_id, 'short_statement_sk')[0];
          $og_url = get_permalink();
-         $og_poster = $poster_medium;
+         $og_poster = $poster_full;
          require_once 'header.php';
      ?>
  </head>
